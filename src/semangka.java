18385203ad13beb65b088b09804753d416cceeeb
farelwahyu <hanafijokowire@gmail.com>
2024-09-29 16:28:00 +0700

    farel

---
+++ src/semangka.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FADHILA
+ */
+public class semangka {
+
+    String nama, warna, bentuk, rasa, ukuran;
+
+    public void setnama_buah(String nama_buah) {
+        this.nama = nama_buah;
+    }
+
+    public String getnama_buah() {
+        return nama;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setbentuk(String bentuk) {
+        this.bentuk = bentuk;
+    }
+
+    public String getbentuk() {
+        return bentuk;
+    }
+
+    public void setrasa(String rasa) {
+        this.rasa = rasa;
+    }
+
+    public String getrasa() {
+        return rasa;
+    }
+
+    public void setukuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+
+    public String getukuran() {
+        return ukuran;
+    }
+    
+
+}
